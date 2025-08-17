07559947fc53ebae4b9306dae64ce1a81120a926
NRacoo <ariaaurar@gmail.com>
2025-08-17 20:21:13 +0700

    fix Role DTO

--- domain/models/role.go
+++ domain/models/role.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Role struct {
-	ID         uint   `gorm:"primaryKey;autoIncrement`
-	Code       string `gorm:"varchar(15);not null`
-	Name       string `gorm;"varchar(15);not null`
+	ID         uint   `gorm:"primaryKey;autoIncrement"`
+	Code       string `gorm:"varchar(15);not null"`
+	Name       string `gorm:"varchar(15);not null"`
 	Created_at *time.Time
 	Updated_at *time.Time
 }
